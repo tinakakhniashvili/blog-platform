0d4bb294681b7afa8381d0c554807f033332a169
tinakakhniashvili <hosktina@gmail.com>
2025-06-02 15:59:42 +0400

    Update services and Docker setup; fix Mongo connection; clean Docker Compose

--- post-service/app.py
+++ post-service/app.py
@@ -1,29 +1,20 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@postgres:5432/posts'
-db = SQLAlchemy(app)
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    content = db.Column(db.Text)
-
-db.create_all()
+app.config["MONGO_URI"] = "mongodb://mongo:27017/postsdb"
+mongo = PyMongo(app)
 
 @app.route('/posts', methods=['POST'])
 def create_post():
     data = request.json
-    post = Post(title=data['title'], content=data['content'])
-    db.session.add(post)
-    db.session.commit()
+    mongo.db.posts.insert_one({'title': data['title'], 'content': data['content']})
     return jsonify({'message': 'Post created'}), 201
 
 @app.route('/posts', methods=['GET'])
 def get_posts():
-    posts = Post.query.all()
-    return jsonify([{'id': p.id, 'title': p.title, 'content': p.content} for p in posts])
+    posts = mongo.db.posts.find()
+    return jsonify([{'title': post['title'], 'content': post['content']} for post in posts])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
